2cbba39ed5d14d99c1d12df645a624995c937868
marty957 <fantuzzimartina@yahoo.it>
2024-12-17 11:07:13 +0100

    modal static

---
+++ src/components/ModalToAddExperience.jsx
@@ -0,0 +1,67 @@
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import Datetime from "react-datetime";
+
+const ModalToAddExperience = (props) => {
+  return (
+    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="Position">
+            <Form.Label column sm="2" required>
+              Role
+            </Form.Label>
+            <Col sm="12">
+              <Form.Control type="text" placeholder="Position role" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="companyName">
+            <Form.Label column sm="2">
+              Company
+            </Form.Label>
+            <Col sm="12">
+              <Form.Control type="text" placeholder="Company Name" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="description">
+            <Form.Label column sm="2">
+              Description
+            </Form.Label>
+            <Col sm="12">
+              <Form.Control type="text" placeholder="Describe your duties" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="area">
+            <Form.Label column sm="2">
+              Area
+            </Form.Label>
+            <Col sm="12">
+              <Form.Control type="text" placeholder="Area" />
+            </Col>
+          </Form.Group>
+          <Row>
+            <Col sm={6}>
+              <Form.Group className="mb-3" controlId="description">
+                <Form.Label>Start Date</Form.Label>
+                <Datetime />
+              </Form.Group>
+            </Col>
+            <Col sm={6}>
+              <Form.Group className="mb-3" controlId="description">
+                <Form.Label>End Date</Form.Label>
+                <Datetime />
+              </Form.Group>
+            </Col>
+          </Row>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalToAddExperience;
--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -6,22 +6,25 @@ import { BiDotsHorizontalRounded } from "react-icons/bi";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import { IoPencil } from "react-icons/io5";
+import { FiPlusCircle } from "react-icons/fi";
+import ModalToAddExperience from "./ModalToAddExperience";
 
 const ProfileDetails = () => {
-  const [profile, setProfile] = useState(null);                                   // Stato per memorizzare i dati del profilo
-  const [isLoading, setIsLoading] = useState(true);                               // Stato per la gestione del caricamento
+  const [profile, setProfile] = useState(null); // Stato per memorizzare i dati del profilo
+  const [isLoading, setIsLoading] = useState(true); // Stato per la gestione del caricamento
+  const [modalShow, setModalShow] = useState(false); // stato per gestione del modale
 
   useEffect(() => {
-    
     // Funzione per recuperare il profilo
     const fetchProfile = async () => {
       try {
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzYwNWU4NDc0YTg2ODAwMTVkYjU0ZjkiLCJpYXQiOjE3MzQzNjg5MDAsImV4cCI6MTczNTU3ODUwMH0.qlKB2g8pPEkFuSrRMQ84ltLLbqQEaT46Vch8Hu9AHiE"; // Assicurati di inserire il token corretto
+        const token =
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzYwNWU4NDc0YTg2ODAwMTVkYjU0ZjkiLCJpYXQiOjE3MzQzNjg5MDAsImV4cCI6MTczNTU3ODUwMH0.qlKB2g8pPEkFuSrRMQ84ltLLbqQEaT46Vch8Hu9AHiE"; // Assicurati di inserire il token corretto
         const response = await fetch("https://striveschool-api.herokuapp.com/api/profile/me", {
           method: "GET",
           headers: {
-            Authorization: `Bearer ${token}`,
-          },
+            Authorization: `Bearer ${token}`
+          }
         });
 
         if (!response.ok) {
@@ -29,19 +32,19 @@ const ProfileDetails = () => {
         }
 
         const data = await response.json();
-        setProfile(data);                                                                  // Salva i dati nel nostro stato
-        setIsLoading(false);                                                               // Imposta loading a false quando i dati sono stati caricati
+        setProfile(data); // Salva i dati nel nostro stato
+        setIsLoading(false); // Imposta loading a false quando i dati sono stati caricati
       } catch (error) {
         console.error("Errore nel recupero del profilo:", error);
-        setIsLoading(false);                                                               // Imposta loading a false anche in caso di errore
+        setIsLoading(false); // Imposta loading a false anche in caso di errore
       }
     };
 
-    fetchProfile();                                                                        // Chiamata per recuperare il profilo
-  }, []);                                                                                  // Il hook viene eseguito solo una volta, al montaggio del componente
+    fetchProfile(); // Chiamata per recuperare il profilo
+  }, []); // Il hook viene eseguito solo una volta, al montaggio del componente
 
   if (isLoading) {
-    return <div>Caricamento...</div>;                                                     // Mostra un messaggio di caricamento finché i dati non sono pronti
+    return <div>Caricamento...</div>; // Mostra un messaggio di caricamento finché i dati non sono pronti
   }
 
   if (!profile) {
@@ -59,15 +62,11 @@ const ProfileDetails = () => {
             style={{ height: 150, width: "100%", objectFit: "cover" }}
           />
         </div>
-        <img
-          src={profile.image}
-          alt="Profile"
-          width={100}
-          height={100}
-          className="ms-4 mt-2"
-        />
+        <img src={profile.image} alt="Profile" width={100} height={100} className="ms-4 mt-2" />
         {/*il nome, il titolo e l'area dell'utente */}
-        <h3 className="ps-4 m-0">{profile.name} {profile.surname}</h3>
+        <h3 className="ps-4 m-0">
+          {profile.name} {profile.surname}
+        </h3>
         <p className="ps-4 fw-semibold m-0">{profile.title}</p>
         <p className="ps-4 mb-1">{profile.area}</p>
         <div className="d-flex gap-3 ms-3 mb-3">
@@ -106,46 +105,54 @@ const ProfileDetails = () => {
       </div>
       <div className="rounded-4 bg-white mt-2 p-3">
         <div className=" d-flex align-items-center fw-semibold mb-2">
-          
-        <p>Attività</p>
-        <div className="d-flex gap-2 ms-auto">
-        <Button className="border-primary rounded-pill text-primary bg-white px-3" size="sm">Crea un post</Button>
-        <p>
-          <IoPencil />
-        </p>
-        </div>
+          <p>Attività</p>
+          <div className="d-flex gap-2 ms-auto">
+            <Button className="border-primary rounded-pill text-primary bg-white px-3" size="sm">
+              Crea un post
+            </Button>
+            <p>
+              <IoPencil />
+            </p>
+          </div>
         </div>
         <div>
           <span className="d-flex gap-2">
-          <Button className="border-secondary rounded-pill text-secondary bg-white px-3" size="sm">Post</Button>
-          <Button className="border-secondary rounded-pill text-secondary bg-white px-3" size="sm">Documenti</Button>
+            <Button className="border-secondary rounded-pill text-secondary bg-white px-3" size="sm">
+              Post
+            </Button>
+            <Button className="border-secondary rounded-pill text-secondary bg-white px-3" size="sm">
+              Documenti
+            </Button>
           </span>
         </div>
         <div className="d-flex align-items-center gap-2 mt-2">
-          <img src="https://images-ext-1.discordapp.net/external/u9_nLQbZ6ALxXIBDBB0QLVMqM7vko1sMCFid-sWX1Qc/https/www.timevision.it/news/wp-content/uploads/2023/04/esperienze-lavorative.jpg?format=webp&width=1191&height=100" alt="work-experience" />
+          <img
+            src="https://images-ext-1.discordapp.net/external/u9_nLQbZ6ALxXIBDBB0QLVMqM7vko1sMCFid-sWX1Qc/https/www.timevision.it/news/wp-content/uploads/2023/04/esperienze-lavorative.jpg?format=webp&width=1191&height=100"
+            alt="work-experience"
+          />
           <div>
-          <p className="fw-semibold mb-0">Attestato corso HTML e CSS competenze base</p>
-          <p>1 pagina</p>
+            <p className="fw-semibold mb-0">Attestato corso HTML e CSS competenze base</p>
+            <p>1 pagina</p>
           </div>
         </div>
       </div>
       <div className="rounded-4 bg-white mt-2 p-3">
         <div className=" d-flex align-items-center fw-semibold">
-        <p>Esperienze</p>
-        <p className="ms-auto">
-          <IoPencil />
-        </p>
+          <p>Esperienze</p>
+          <div className="ms-auto">
+            <Button variant="light" className="p-1 border-0 " onClick={() => setModalShow(true)}>
+              <FiPlusCircle className="fs-4 me-2" />
+            </Button>
+            <ModalToAddExperience show={modalShow} onHide={() => setModalShow(false)} />
+            <IoPencil />
+          </div>
         </div>
         <div>
           <span>
-            Architetto con esperienza nella progettazione e realizzazione di
-            spazi innovativi e sostenibili. Mi occupo di progettazione
-            architettonica, pianificazione urbana e restauro, con un forte focus
-            sulla funzionalità, l'estetica e l'efficienza energetica. Ho una
-            solida esperienza nella gestione di progetti complessi, dalla fase
-            di concept fino alla realizzazione, collaborando con team
-            multidisciplinari per garantire il rispetto delle normative e delle
-            tempistiche.
+            Architetto con esperienza nella progettazione e realizzazione di spazi innovativi e sostenibili. Mi occupo di progettazione architettonica,
+            pianificazione urbana e restauro, con un forte focus sulla funzionalità, l'estetica e l'efficienza energetica. Ho una solida esperienza nella
+            gestione di progetti complessi, dalla fase di concept fino alla realizzazione, collaborando con team multidisciplinari per garantire il rispetto
+            delle normative e delle tempistiche.
           </span>
         </div>
       </div>
