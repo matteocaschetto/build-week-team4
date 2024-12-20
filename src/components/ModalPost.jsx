dcf17ab4a3efb8397ff10722d3da4382674e17d4
matteocaschetto <matteocaschetto9@gmail.com>
2024-12-20 12:56:35 +0100

    img post dinamiche

--- src/components/ModalPost.jsx
+++ src/components/ModalPost.jsx
@@ -3,48 +3,61 @@ import { Alert, Button, Col, Form, Modal, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 
 const ModalPost = (props) => {
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState({ text: "", URL: "" });
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const dispatch = useDispatch();
 
-  console.log(post);
-
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzYwNWU4NDc0YTg2ODAwMTVkYjU0ZjkiLCJpYXQiOjE3MzQzNjg5MDAsImV4cCI6MTczNTU3ODUwMH0.qlKB2g8pPEkFuSrRMQ84ltLLbqQEaT46Vch8Hu9AHiE";
+
   const createPost = (e) => {
     e.preventDefault();
-    fetch(`https://striveschool-api.herokuapp.com/api/posts/`, {
+
+    // Verifica che l'URL sia valido
+    if (!post.URL || !post.URL.startsWith("http")) {
+      setErrorMessage("L'URL dell'immagine non è valido");
+      return;
+    }
+
+    fetch("https://striveschool-api.herokuapp.com/api/posts/", {
       method: "POST",
       body: JSON.stringify({
-        text: `${post.text}`,
-        URL: `${post.URL}`
+        text: post.text,
+        image: post.URL // Assicurati che l'URL venga passato correttamente
       }),
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`
       }
     })
-      .then((resp) => {
-        if (resp.ok) {
-          console.log("iniviata");
-          setSuccessMessage("post aggiunto");
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (data._id) {
+          setSuccessMessage("Post aggiunto!");
           setErrorMessage("");
-          dispatch({ type: "CREATE_POST", payload: resp });
-          console.log("PostFetch", resp);
+          dispatch({ type: "CREATE_POST", payload: data }); // Aggiungi il post al Redux store
         } else {
-          setErrorMessage("errore");
+          setErrorMessage("Errore durante la creazione del post!");
         }
       })
       .catch((err) => {
-        console.log(err);
+        console.error(err);
+        setErrorMessage("Errore durante la creazione del post!");
       });
   };
 
   return (
-    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Crea un Nuovo Post!</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Crea un Nuovo Post!
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
@@ -75,67 +88,6 @@ const ModalPost = (props) => {
               />
             </Col>
           </Form.Group>
-          {/*<Form.Group as={Row} className="mb-3" controlId="description">
-            <Form.Label column sm="2">
-              Post Description
-            </Form.Label>
-            <Col sm="12">
-              <Form.Control
-                type="text"
-                placeholder="Inserisci la descrizione del tuo post"
-                required
-                value={experience.description}
-                onChange={(e) => setExperience({ ...experience, description: e.target.value })}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-3" controlId="area">
-            <Form.Label column sm="2">
-              Area
-            </Form.Label>
-            <Col sm="12">
-              <Form.Control
-                type="text"
-                placeholder="Area"
-                required
-                value={experience.area}
-                onChange={(e) => setExperience({ ...experience, area: e.target.value })}
-              />
-            </Col>
-          </Form.Group>
-          <Row>
-            <Col sm={6}>
-              <Form.Group className="mb-3" controlId="description">
-                <Form.Label>Start Date</Form.Label>
-                <Form.Control
-                  type="date"
-                  required
-                  value={experience.startDate}
-                  onChange={(e) =>
-                    setExperience({
-                      ...experience,
-                      startDate: e.target.value.toString()
-                    })
-                  }
-                />
-              </Form.Group>
-            </Col>
-            <Col sm={6}>
-              <Form.Group className="mb-3" controlId="description">
-                <Form.Label>End Date</Form.Label>
-                <Form.Control
-                  type="date"
-                  value={experience.endDate}
-                  onChange={(e) =>
-                    setExperience({
-                      ...experience,
-                      endDate: e.target.value.toString()
-                    })
-                  }
-                />
-              </Form.Group>
-            </Col>
-          </Row>*/}
         </Form>
       </Modal.Body>
       <Modal.Footer>
@@ -146,4 +98,5 @@ const ModalPost = (props) => {
     </Modal>
   );
 };
+
 export default ModalPost;
